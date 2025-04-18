d269be2ebfca1399bc1d89b020032a16aab89f25
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-04-18 16:32:50 +0200

    add doctor_picture

--- rating/serializers.py
+++ rating/serializers.py
@@ -5,7 +5,7 @@ class DoctorReviewSerializer(serializers.ModelSerializer):
     patient_username = serializers.CharField(source='patient.username', read_only=True)
     doctor_name = serializers.CharField(source='doctor.username', read_only=True)
     patient_picture = serializers.ImageField(source='patient.profile_picture', read_only=True)
-    doctor_picture = serializers.CharField(source='doctor.username', read_only=True)
+    doctor_picture = serializers.CharField(source='doctor.profile_picture', read_only=True)
     first_name = serializers.CharField(source='patient.first_name', read_only=True)
     last_name = serializers.CharField(source='patient.last_name', read_only=True)
     class Meta:
